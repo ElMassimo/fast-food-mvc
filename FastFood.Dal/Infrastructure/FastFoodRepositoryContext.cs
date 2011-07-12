1ae35472d7e1b6fe6b916e9a7de6cfcdaccae2f4
maximomussini@gmail.com <maximomussini@gmail.com@94608daf-2a64-bded-8ab9-09a3fffdb8e1>
2011-07-12 23:03:30 +0000

    Dal

---
+++ FastFood.Dal/Infrastructure/FastFoodRepositoryContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Objects;
+
+namespace FastFood.Dal.Infrastructure
+{
+    public class SisocanaRepositoryContext : IRepositoryContext
+    {
+        private const string OBJECT_CONTEXT_KEY = "FastFood.Dal.EntityModels.FastFoodEntitiesContainer";
+        public IObjectSet<T> GetObjectSet<T>() 
+            where T : class
+        {
+            return ContextManager.GetObjectContext(OBJECT_CONTEXT_KEY).CreateObjectSet<T>();
+        }
+
+        /// <summary>
+        /// Returns the active object context
+        /// </summary>
+        public ObjectContext ObjectContext
+        {
+            get
+            {
+                return ContextManager.GetObjectContext(OBJECT_CONTEXT_KEY);
+            }
+        }
+
+        public int SaveChanges()
+        {
+            return this.ObjectContext.SaveChanges();
+        }
+
+        public void Terminate()
+        {
+            ContextManager.SetRepositoryContext(null, OBJECT_CONTEXT_KEY);
+        }
+
+    }
+}
