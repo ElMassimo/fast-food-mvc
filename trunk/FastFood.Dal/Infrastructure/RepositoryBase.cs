d0536cce81f7be48cac501cdc57008a93cb5b294
maximomussini@gmail.com <maximomussini@gmail.com@94608daf-2a64-bded-8ab9-09a3fffdb8e1>
2011-07-12 23:03:30 +0000

    Dal

---
+++ trunk/FastFood.Dal/Infrastructure/RepositoryBase.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+using System.Data.Objects;
+
+namespace FastFood.Dal.Infrastructure
+{
+    public abstract class RepositoryBase<T> : IRepository<T>
+        where T : class
+    {
+        private IRepositoryContext RepositoryContext;
+
+        public RepositoryBase()
+            : this(new SisocanaRepositoryContext())
+        {
+        }
+
+        public RepositoryBase(IRepositoryContext repositoryContext)
+        {
+            repositoryContext = repositoryContext ?? new SisocanaRepositoryContext();
+            RepositoryContext = repositoryContext;
+            _objectSet = repositoryContext.GetObjectSet<T>();
+        }
+
+        private IObjectSet<T> _objectSet;
+        public IObjectSet<T> ObjectSet
+        {
+            get
+            {
+                return _objectSet;
+            }
+        }
+
+        #region IRepository Members
+
+        public void Add(T entity)
+        {
+            this.ObjectSet.AddObject(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            this.ObjectSet.DeleteObject(entity);
+        }
+
+        public IList<T> GetAll()
+        {
+            return this.ObjectSet.ToList<T>();
+        }
+
+        public IList<T> GetAll(Expression<Func<T, bool>> whereCondition)
+        {
+            return this.ObjectSet.Where(whereCondition).ToList<T>();
+        }
+
+        public T GetSingle(Expression<Func<T, bool>> whereCondition)
+        {
+            return this.ObjectSet.Where(whereCondition).FirstOrDefault<T>();
+        }
+
+        public void Attach(T entity)
+        {
+            this.ObjectSet.Attach(entity);
+        }
+
+        public IQueryable<T> GetQueryable()
+        {
+            return this.ObjectSet.AsQueryable<T>();
+        }
+
+        public long Count()
+        {
+            return this.ObjectSet.LongCount<T>();
+        }
+
+        public long Count(Expression<Func<T, bool>> whereCondition)
+        {
+            return this.ObjectSet.Where(whereCondition).LongCount<T>();
+        }
+
+        public void Save()
+        {
+            RepositoryContext.SaveChanges();
+        }
+
+        #endregion
+
+    }
+}
---
+++ trunk/FastFood.Dal/Repositories/OrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using FastFood.Dal.EntityModels;
+using FastFood.Dal.Infrastructure;
+
+namespace FastFood.Dal.Repositories
+{
+    public interface IOrderRepository : IRepository<Order>
+    {
+
+    }
+
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+    }
+}
